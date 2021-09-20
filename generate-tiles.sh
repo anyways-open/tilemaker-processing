afaeb14c5e0546c2bac889109cc3ce9c19b5eec0
Ben Abelshausen <ben@itinero.tech>
2021-09-20 17:32:08 +0200

    Updated generate tiles script.

--- generate-tiles.sh
+++ generate-tiles.sh
@@ -8,4 +8,18 @@ wget https://staging.anyways.eu/planet/planet/extracts/belgium-and-neighbourhood
 
 # generate tiles.
 mkdir out
-docker run -v ${ROOT}:/in -v ${ROOT}/out:/out -v ${ROOT}/coastline/:/coastline -v ${ROOT}/config/:/config -i -t --rm anywaysopen/tilemaker:staging /in/belgium-and-neighbourhood-bbox-latest.osm.pbf --output=/out/belgium-and-neighbourhood-bbox.mbtiles --process=/config/process-openmaptiles.lua
+docker run -v ${ROOT}:/in -v ${ROOT}/out:/out -v ${ROOT}/coastline/:/coastline -v ${ROOT}/config/:/config -i -t --rm anywaysopen/tilemaker:staging /in/belgium-and-neighbourhood-bbox-latest.osm.pbf --output=/out/openmaptiles.mbtiles --process=/config/process-openmaptiles.lua
+
+# rename old files
+olddate=$(date -r "${OUTPUT}/openmaptiles.mbtiles" "+%Y%m%d")
+rm ${OUTPUT}/openmaptiles-${olddate}.mbtiles
+rm ${OUTPUT}/openmaptiles-${olddate}.mbtiles.md5
+mv ${OUTPUT}/openmaptiles.mbtiles ${OUTPUT}/openmaptiles-${olddate}.mbtiles
+mv ${OUTPUT}/openmaptiles.mbtiles.md5 ${OUTPUT}/openmaptiles-${olddate}.mbtiles.md5
+
+# move files to output folder.
+mv ${ROOT}/out/openmaptiles.mbtiles ${OUTPUT}/openmaptiles.mbtiles
+cd ${OUTPUT}
+md5sum openmaptiles.mbtiles > openmaptiles.mbtiles.md5
+
+cd ${ROOT}
