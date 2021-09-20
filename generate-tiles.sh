2085d00be0673f5bddc30d2309f3c3f5a50d61c1
Ben Abelshausen <ben@itinero.tech>
2021-09-20 17:38:14 +0200

    Enabled tile generation again.

--- generate-tiles.sh
+++ generate-tiles.sh
@@ -3,12 +3,12 @@ ROOT=$PWD
 OUTPUT=$1
 
 # download latest dump.
-# rm belgium-and-neighbourhood-bbox-latest.osm.pbf
-# wget https://staging.anyways.eu/planet/planet/extracts/belgium-and-neighbourhood-bbox-latest.osm.pbf
+rm belgium-and-neighbourhood-bbox-latest.osm.pbf
+wget https://staging.anyways.eu/planet/planet/extracts/belgium-and-neighbourhood-bbox-latest.osm.pbf
 
 # generate tiles.
 mkdir out
-# docker run -v ${ROOT}:/in -v ${ROOT}/out:/out -v ${ROOT}/coastline/:/coastline -v ${ROOT}/config/:/config -i -t --rm anywaysopen/tilemaker:staging /in/belgium-and-neighbourhood-bbox-latest.osm.pbf --output=/out/openmaptiles.mbtiles --process=/config/process-openmaptiles.lua
+docker run -v ${ROOT}:/in -v ${ROOT}/out:/out -v ${ROOT}/coastline/:/coastline -v ${ROOT}/config/:/config -i -t --rm anywaysopen/tilemaker:staging /in/belgium-and-neighbourhood-bbox-latest.osm.pbf --output=/out/openmaptiles.mbtiles --process=/config/process-openmaptiles.lua
 
 # rename old files
 olddate=$(date -r "${OUTPUT}/openmaptiles.mbtiles" "+%Y%m%d")
