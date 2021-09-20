a0ac327773df247ebf642d5261daae2b66c25952
Ben Abelshausen <ben@itinero.tech>
2021-09-20 16:34:30 +0200

    Write tiles to output folder.

--- generate-tiles.sh
+++ generate-tiles.sh
@@ -7,4 +7,5 @@ rm belgium-and-neighbourhood-bbox-latest.osm.pbf
 wget https://staging.anyways.eu/planet/planet/extracts/belgium-and-neighbourhood-bbox-latest.osm.pbf
 
 # generate tiles.
-docker run -v ${ROOT}:/in -v ${OUTPUT}:/out -v ${ROOT}/coastline/:/coastline -v ${ROOT}/config/:/config -i -t --rm anywaysopen/tilemaker:staging /in/belgium-and-neighbourhood-bbox-latest.osm.pbf --output=/out/belgium-and-neighbourhood-bbox.mbtiles --process=/config/process-openmaptiles.lua
+mkdir out
+docker run -v ${ROOT}:/in -v ${ROOT}/out:/out -v ${ROOT}/coastline/:/coastline -v ${ROOT}/config/:/config -i -t --rm anywaysopen/tilemaker:staging /in/belgium-and-neighbourhood-bbox-latest.osm.pbf --output=/out/belgium-and-neighbourhood-bbox.mbtiles --process=/config/process-openmaptiles.lua
