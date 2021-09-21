680474201c4ddd27522b6eb7717082248c4c63ef
Ben Abelshausen <ben@itinero.tech>
2021-09-21 12:42:24 +0200

    Coastline instead of coastlines.

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -3,5 +3,5 @@ ROOT=$PWD
 
 rm water-polygons-split-4326.zip 
 wget https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip 
-unzip -j -o -d ./coastlines/ water-polygons-split-4326.zip 
+unzip -j -o -d ./coastline/ water-polygons-split-4326.zip 
 rm water-polygons-split-4326.zip 
