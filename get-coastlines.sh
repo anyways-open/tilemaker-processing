82d55baafc2272554a01f78f004802ce75eeba08
Ben Abelshausen <ben@itinero.tech>
2021-09-29 15:26:22 +0200

    Added urban areas.

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -5,3 +5,9 @@ rm water-polygons-split-4326.zip
 wget https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip 
 unzip -j -o -d ./coastline/ water-polygons-split-4326.zip 
 rm water-polygons-split-4326.zip 
+
+mkdir -p landcover/ne_10m_urban_areas
+cd landcover/ne_10m_urban_areas && rm -rf ne_10m_urban_areas.zip
+cd landcover/ne_10m_urban_areas && wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_urban_areas.zip
+cd landcover/ne_10m_urban_areas && unzip -o ne_10m_urban_areas.zip
+
