31ab112fc4acb10a3df63793df987d58510c0525
Ben Abelshausen <ben@itinero.tech>
2021-09-29 17:13:12 +0200

    Get landcover.

--- get-coastlines.sh
+++ get-coastlines.sh
@@ -9,6 +9,21 @@ wget https://staging.anyways.eu/planet/naturalearth/ne_10m_urban_areas.zip
 unzip -o ne_10m_urban_areas.zip
 cd $ROOT
 
+# get ice shelves.
+mkdir -p landcover/ne_10m_antarctic_ice_shelves_polys
+cd landcover/ne_10m_antarctic_ice_shelves_polys 
+rm -rf ne_10m_antarctic_ice_shelves_polys.zip
+wget https://staging.anyways.eu/planet/naturalearth/ne_10m_antarctic_ice_shelves_polys.zip
+unzip -o ne_10m_antarctic_ice_shelves_polys.zip
+cd $ROOT
+
+# get glaciated areas.
+mkdir -p landcover/ne_10m_glaciated_areas
+cd landcover/ne_10m_glaciated_areas 
+rm -rf ne_10m_glaciated_areas.zip
+wget https://staging.anyways.eu/planet/naturalearth/ne_10m_glaciated_areas.zip
+unzip -o ne_10m_glaciated_areas.zip
+
 # get water polygons.
 rm water-polygons-split-4326.zip 
 wget https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip 
