87a43bd006312fd962c58040d0908c75016b5e5c
Ben Abelshausen <ben@itinero.tech>
2021-10-04 12:34:15 +0200

    WIP on pedestrian areas.

--- config/process-openmaptiles.lua
+++ config/process-openmaptiles.lua
@@ -121,7 +121,7 @@ mainRoadValues  = Set { "secondary", "motorway_link", "trunk_link", "primary_lin
 midRoadValues   = Set { "tertiary", "tertiary_link" }
 minorRoadValues = Set { "unclassified", "residential", "road", "living_street" }
 trackValues     = Set { "cycleway", "byway", "bridleway", "track" }
-pathValues      = Set { "footway", "path", "steps" }
+pathValues      = Set { "footway", "path", "steps", "pedestrian" }
 linkValues      = Set { "motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link" }
 constructionValues = Set { "primary", "secondary", "tertiary", "motorway", "service", "trunk", "track" }
 
@@ -219,6 +219,7 @@ function way_function(way)
 	local man_made = way:Find("man_made")
 	local boundary = way:Find("boundary")
 	local isClosed = way:IsClosed()
+	local isArea = isClosed and way.Find("area") == "yes"
 	local housenumber = way:Find("addr:housenumber")
 	local write_name = false
 	local construction = way:Find("construction")
